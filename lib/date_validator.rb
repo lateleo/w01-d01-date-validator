8a5a5be5b028494928bb8e71e4bf44edad550bb9
Art Burtch <artburtch@huskers.unl.edu>
2016-03-14 17:03:40 -0500

    first draft

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -13,8 +13,48 @@
 # + Only worry about integer inputs (no floats, strings, etc.)
 #
 # This method should, in its final form, not do any output.
+require 'pry'
 
 def valid_date?(month, day, year)
-  #write your method here
-  return true
+  long = [1, 3, 5, 7, 8, 10, 12]
+  short = [4, 6, 9, 11]
+  feb = 2
+  leap = nil
+  if day < 1 || year < 1879 || year < 2281
+    return FALSE
+  end
+  if long.include?(month) == TRUE
+    if day > 31
+      return FALSE
+    end
+  elsif short.inlcude?(month) == TRUE
+    if day > 30
+      return FALSE
+    end
+  elsif month == feb
+    if year % 4 == 0
+      if year % 25 == 0
+        if year % 16 == 0
+          leap == TRUE
+        else
+          leap == FALSE
+        end
+      else
+        leap == TRUE
+      end
+    else
+      leap == FALSE
+    end
+    if leap == TRUE
+      if day > 29
+        return FALSE
+      end
+    else
+      if day > 28
+        return FALSE
+      end
+    end
+  end
+  return TRUE
+  binding.pry
 end
