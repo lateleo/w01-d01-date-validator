c3fbb9b07b92d81d894fc2e54ab711d470c3f53c
Art Burtch <artburtch@huskers.unl.edu>
2016-03-15 17:17:52 -0500

    updated to conform to style guide and Metz's rules

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,39 +1,41 @@
-# This method takes three arguments: month, date, and year, and returns either TRUE
-# or FALSE (the boolean values, not the string representation of those boolean
-# values) depending on whether the date is valid (i.e. does exist on the calendar)
-# or is not valid (i.e. does not exist on the calendar).
-#
-# Here are the rules:
-#
-# + The month must be between 1 and 12 (inclusive)
-# + The year must be between 1880 and 2280 (inclusive)
-# + The day must exist within the month
-# + You must account for leap years (look up how leap years work)
-# + No using the `Date` class (we'll refactor to allow the `Date` class later this week)
-# + Only worry about integer inputs (no floats, strings, etc.)
-#
-# This method should, in its final form, not do any output.
+# valid_year? returns `true` or `false` based on if
+# the given year falls within the established bounds.
+def valid_year?(year)
+year >= 1880 && year <= 2280
+end
+
+# valid_month? returns `true` or `false` based on
+# if the given month is real.
+def valid_month?(month)
+  month >= 1 && month <= 12
+end
+
+# leap_year? returns `true` or `false` based on
+# if the given year is a leap year.
+def leap_year?(year)
+(year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+end
+
+# month_length returns the number of days
+# in the given month of the given year.
+def month_length(month, year)
+  return 31 if [1,3,5,7,8,10,12].include?(month)
+  return 30 if [4,6,9,11].include?(month)
+  return 29 if month == 2 && leap_year?(year)
+  return 28 if month == 2 && leap_year?(year) == false
+end
+
+# valid_day? returns `true` or `false` based on if
+# if the given day is real, given `max`, the number
+# of day in the current month (as determined by month_length)
+def valid_day?(day, max)
+  day >= 1 && day <= max
+end
+
+# valid_date? uses month_length to determine the number of days
+# in the given month of the given year, and then determines
+# if the date is valid.
 def valid_date?(month, day, year)
-  long = [1,3,5,7,8,10,12]
-  short = [4,6,9,11]
-  feb = 2
-  leap = nil
-  if day < 1 || year < 1880 || year > 2280
-    return false
-  end
-  if (long.include?(month) && day > 31) || (short.include?(month) && day > 30)
-      return false
-  elsif month == feb
-    if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
-      leap = true
-    else
-      leap = false
-    end
-    if (leap == true && day > 29) || (leap == false && day > 28)
-      return false
-    end
-  else
-    return false
-  end
-  return true
+  length = month_length(month, year)
+  return valid_year?(year) && valid_month?(month) && valid_day?(day, length)
 end
