4cd2a08e6c93fd296961e90dec1cb8f794f13458
Art Burtch <artburtch@huskers.unl.edu>
2016-03-17 19:54:48 -0500

    removed returns, added ternary

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -2,15 +2,14 @@
 # Returns the number of days in February
 # of the given year, based on whether or not it's a leap year.
 def feb_length(year)
-  return 29 if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
-  28
+  (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 29 : 28
 end
 
 # Determines if the date is valid.
 def valid_date?(month, day, year)
   length = [31,feb_length(year),31,30,31,30,31,31,30,31,30,31]
-  return (1880..2280).cover?(year) &&
-         (1..12).cover?(month) &&
-         (1..length[month.pred].to_i).cover?(day)
-         # .to_i ensures that a value of nil will be converted to 0.
+  (1880..2280).cover?(year) &&
+  (1..12).cover?(month) &&
+  (1..length[month.pred].to_i).cover?(day)
+  # .to_i ensures that a value of nil will be converted to 0.
 end
